9f1bd0dfc826bb399f244814dd6fa35cf00c8bc3
AnjumRaza464 <hafizanjumraza1993@gmail.com>
2025-08-13 14:49:13 +0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medi_bnbchatbot",
+    version="0.1.0",
+    author="Anjum Raza",
+    author_email="hafizanjumraza1993@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
